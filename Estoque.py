84475731f645349e1d0fc2541db2ba07d589d8ce
Mickael Castro <mickaelcastro15@gmail.com>
2023-12-07 15:57:25 -0300

    Implementação de Registro de Estoque

--- Estoque.py
+++ Estoque.py
@@ -1,8 +1,52 @@
+import sys
 class Estoque:
     def __init__(self):
-        self.catalogo = {"arroz": 10.8, "Feijao": 8.5, "macarrão": 5.8, "carne": 25.0}
+        self.catalogo = {"arroz": 10.8, "feijao": 8.5, "macarrão": 5.8, "carne": 25.0}
         self.carrinho = {}
         self.total = 0.0
+        self.funcionalidades = ["Comprar", "Cadastro", "Relatorio", "Controle"]
+
+
+    def esolha_funcao(self):
+        while True:
+            print("Escolha entre as opções abaixo de funcionalidade: ")
+            for funcionalidades in self.funcionalidades:
+                print(f'{funcionalidades.capitalize()}')
+
+            escolha = input("Digite qual das funcionalidade você deseja. (ou ´sair´ para Cancelar): ")
+
+            if escolha.lower() == "sair":
+                break
+
+            if escolha in self.funcionalidades:
+                if escolha.lower() == "comprar":
+                    self.comprar()
+                elif escolha.lower() == "cadastro":
+                    self.cadastro_produto()
+            else:
+                print("Não encontrei tal funcionalidade, ou não implementamos ainda :(")
+                print("Tente Novamente")
+                self.esolha_funcao()
+
+    def cadastro_produto(self):
+        while True:
+            novo_produto = input("Digite o produto a cadastrar. (ou 'sair' para cancelar): ")
+
+            if novo_produto.lower() == "sair":
+                break
+
+            novo_preco = input("Digite o preco deste produto. (ou 'sair' para cancelar): ")
+
+            if novo_preco.lower() == "sair":
+                break
+
+            # Convertendo o preço para float
+            preco = float(novo_preco)
+
+            # Atualizando o catálogo
+            self.catalogo.update({novo_produto: preco})
+            for produto, preco in self.catalogo.items():
+                print(f'{produto.capitalize()} - R${preco}')
 
     def comprar(self):
         while True:
@@ -11,6 +55,7 @@ class Estoque:
                 print(f'{produto.capitalize()} - R${preco}')
 
             escolha = input("Digite o nome do produto que deseja comprar (ou `sair` para encerrar): ")
+
             if escolha.lower() == "sair":
                 break
 
@@ -60,13 +105,21 @@ class Estoque:
         if cpf_nota.lower() == "sim":
             cpf = input("Digite o CPF: ")
             print(f"Nota fiscal: Total da compra - R${self.total} | CPF - {cpf}")
+
         else:
             print(f"Nota fiscal: Total da compra - R${self.total}")
 
+        escolha2 = input("Você deseja continuar?(sim ou nao)")
 
-        self.catalogo = {"arroz": 10.8, "Feijao": 8.5, "macarrão": 5.8, "carne": 25.0}
-        self.carrinho = {}
-        self.total = 0.0
+        if escolha2.lower() == "nao": #escolha de continuação do código
+            sys.exit()
+        else:
+            self.esolha_funcao()
 
-mercado = Estoque()
-mercado.comprar()
+
+
+
+
+if __name__ == "__main__":
+    estoque = Estoque()
+    estoque.esolha_funcao()
