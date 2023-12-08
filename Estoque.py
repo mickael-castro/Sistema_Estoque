b504c70c4a440012d2175d22d06a5c02259c1939
Maria Clara de Oliveira <mariaclaraminion@gmail.com>
2023-12-07 23:07:07 -0300

    adição de controle

--- Estoque.py
+++ Estoque.py
@@ -6,13 +6,13 @@ id = 0
 
 @unique
 class Funcionalidade(IntEnum):
-    COMPRAR = 1
+    VENDER = 1
     CADASTRO = 2
     RELATORIO = 3
     CONTROLE = 4
 
 class Produto:
-    def __init__(self, nome, preco, validade, quantidade):
+    def __init__(self, nome, preco, validade=None, quantidade=int):
         self.modificar_id()
         self.id = id
 
@@ -30,17 +30,21 @@ class Estoque:
         self.carrinho = []
         self.total = 0.0
         self.produtos = []
+        self.catalogo = []
 
-        arroz = Produto('Saco de Arroz (1 Kg)', 10.8, date(2023, 12, 10), 10)
+        arroz = Produto('Saco de Arroz (1 Kg)', 10.8, date(2023, 12, 1), 10)
         self.produtos.append(arroz)
 
         feijao = Produto('Saco de Feijao (1 Kg)', 8.5, date(2023, 12, 12), 10)
         self.produtos.append(feijao)
 
-        macarrao = Produto('Saco de Macarrão (500 g)', 5.8, date(2023, 12, 7), 10)
+        macarrao = Produto('Saco de Macarrão (500 g)', 5.8, date(2023, 12, 1), 10)
         self.produtos.append(macarrao)
 
         carne = Produto('Peça de maminha (1 Kg)', 25.0, date(2023, 12, 10), 10)
+        self.produtos.append(carne)
+
+        pano = Produto('Pano de prato "Me traga café que eu me expresso"', 50.0, None, 10)
         self.produtos.append(carne)
 
     def escolha_funcao(self):
@@ -49,9 +53,9 @@ class Estoque:
             for funcionalidade in Funcionalidade:
                 print(f'{funcionalidade.value} - {funcionalidade.name.capitalize()}')
 
-            escolha = input("Digite o número correspondente à funcionalidade que você deseja usar ou 'sair' para Cancelar: ")
+            escolha = input("Digite o número correspondente à funcionalidade que você deseja usar ou '0' para Cancelar: ")
 
-            if escolha.lower() == "sair":
+            if escolha == "0":
                 break
 
             try:
@@ -60,10 +64,12 @@ class Estoque:
                 print("Digite um número válido.")
                 continue
 
-            if escolha == Funcionalidade.COMPRAR.value:
-                self.comprar()
+            if escolha == Funcionalidade.VENDER.value:
+                self.vender()
             elif escolha == Funcionalidade.CADASTRO.value:
                 self.cadastro_produto()
+            elif escolha == Funcionalidade.CONTROLE.value:
+                self.controle()
             else:
                 print("Funcionalidade não encontrada ou não implementada :(")
 
@@ -71,9 +77,9 @@ class Estoque:
         from datetime import datetime
 
         while True:
-            novo_produto = input("Digite o nome do produto a cadastrar (ou 'sair' para cancelar): ")
+            novo_produto = input("Digite o nome do produto a cadastrar (ou '0' para cancelar): ")
 
-            if novo_produto.lower() == "sair":
+            if novo_produto == "0":
                 break
 
             novo_preco = input("Digite o preço deste produto: ")
@@ -83,12 +89,16 @@ class Estoque:
                 print("Digite um valor válido para o preço.")
                 continue
 
-            data_validade_str = input("Digite a data de validade no formato (DD/MM/AAAA): ")
-            try:
-                data_validade = datetime.strptime(data_validade_str, "%d/%m/%Y").date()
-            except ValueError:
-                print("Digite uma data válida no formato correto (DD/MM/AAAA).")
-                continue
+            data_validade_str = input(
+                "Digite a data de validade no formato (DD/MM/AAAA) ou pressione Enter para não definir uma data de validade: ")
+            if data_validade_str.strip():  # Verifica se foi fornecida uma data de validade
+                try:
+                    data_validade = datetime.strptime(data_validade_str, "%d/%m/%Y").date()
+                except ValueError:
+                    print("Digite uma data válida no formato correto (DD/MM/AAAA).")
+                    continue
+            else:
+                data_validade = None  # Define como None se nenhuma data for fornecida
 
             nova_quantidade = input("Digite a quantidade deste produto: ")
             try:
@@ -102,16 +112,16 @@ class Estoque:
 
             print(f'Produto {produto.nome} cadastrado com sucesso.')
 
-    def comprar(self):
+    def vender(self):
         while True:
             print("Catálogo de produtos:")
             print("ID  | Quantidade | Nome do Produto             | Preço      | Validade ")
             for produto in self.produtos:
                 print(
                     f"{produto.id:2} | {produto.quantidade:2} | {produto.nome:30} | R${produto.preco:.2f} | {produto.validade.strftime('%d/%m/%Y')}")
-            escolha = input("Digite o id do produto que deseja comprar (ou `sair` para encerrar): ")
+            escolha = input("Digite o id do produto que deseja vender (ou `0` para encerrar): ")
 
-            if escolha.lower() == "sair":
+            if escolha == "0":
                 break
 
             escolha = int(escolha)
@@ -124,14 +134,24 @@ class Estoque:
                     break  # Para o loop após encontrar o produto correspondente
 
             if selecao is not None:
-                qtd_desejada = int(input("Digite a quantidade desejada: "))
+                qtd_desejada = input("Digite a quantidade desejada (ou `0` para cancelar seleção de produto): ")
 
-                if qtd_desejada > selecao.quantidade:
-                    qtd_desejada = int(input(f"Há apenas {selecao.quantidade} unidades de {selecao.nome}. Digite novamente a quantidade desejada (ou `sair` para cancelar seleção de produto):"))
-                else:
+                if qtd_desejada == "0":
+                    print("Seleção de produto cancelada.")
+                    continue
+
+                qtd_desejada = int(qtd_desejada)
+
+                while qtd_desejada > selecao.quantidade:
+                    qtd_desejada = int(input(f"Há apenas {selecao.quantidade} unidades de {selecao.nome}. "
+                                             f"Digite novamente a quantidade desejada (ou `0` para cancelar seleção de produto):"))
+                    if qtd_desejada == 0:
+                        print("Seleção de produto cancelada.")
+                        break
+                if qtd_desejada != 0:
                     # Atualiza a quantidade no catálogo
                     selecao.quantidade -= qtd_desejada
-                    self.carrinho.append(selecao)
+                    self.carrinho.append((selecao, qtd_desejada))
                     print(f"{qtd_desejada} unidades de {selecao.nome} adicionadas ao carrinho. ")
 
             else:
@@ -139,20 +159,68 @@ class Estoque:
 
         print("\nResumo da compra")
         print("ID  | Quantidade | Nome do Produto             | Preço Unitário | Preço Total ")
-        for produto in self.carrinho:
-            preco_total = produto.preco * qtd_desejada
+        for produto, quantidade in self.carrinho:
+            preco_total = produto.preco * quantidade
             print(
-                f"{produto.id:2} | {qtd_desejada:2} | {produto.nome:30} | R${produto.preco:.2f} | R${preco_total:.2f} ")
+                f"{produto.id:2} | {quantidade:2} | {produto.nome:30} | R${produto.preco:.2f} | R${preco_total:.2f} ")
 
             self.total += preco_total
 
         print(f"Total da Compra: R${self.total}")
-        escolha2 = input("Você deseja continuar?(sim ou nao)")
 
-        if escolha2.lower() == "nao": #escolha de continuação do código
+        confirmacao = input("Você deseja confirmar a venda? (s ou n)")
+
+        if confirmacao == "s":
+            self.carrinho = []
+            #fazer registro de venda
+            print("A venda foi registrada com sucesso")
+        elif confirmacao == "n":
+
+            self.carrinho = []
+
+
+        escolha2 = input("Você deseja escolher outra funcionaldiade? (s ou n)")
+
+        if escolha2.lower() == "n": #escolha de continuação do código
             sys.exit()
-        else:
+        elif escolha2.lower() == "s":
             self.escolha_funcao()
+
+    def controle(self):
+        from datetime import date
+
+        agora = date.today()
+        for produto in self.produtos:
+            if produto.validade < agora:
+                print(
+                    f"{produto.id:2} | {produto.quantidade:2} | {produto.nome:30} | R${produto.preco:.2f} | {produto.validade.strftime('%d/%m/%Y')}")
+
+        delete = input("Você deseja deletar o registro dos produtos inválidos? (s ou n)")
+
+        if delete == 's':
+            print("deletou")
+        elif delete == 'n':
+            print("n deletou")
+        else:
+            print("Não há produtos fora da validade")
+
+        escolha = input('Você deseja deletar algum produto mesmo assim? (s ou n)')
+        if escolha.lower() == 's':
+            self.deletar()
+
+            # elif escolha.lower() == 'n':
+            #     escolha2 = input("Você deseja escolher outra funcionaldiade? (s ou n)")
+            #
+            #     if escolha2.lower() == "n":  # escolha de continuação do código
+            #         sys.exit()
+            #     elif escolha2.lower() == "s":
+            #         self.escolha_funcao()
+
+    def deletar(self):
+        print('entrou')
+        pass
+
+
 
 if __name__ == "__main__":
     estoque = Estoque()
